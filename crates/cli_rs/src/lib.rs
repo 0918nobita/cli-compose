7e74691988d3fdea8addbbeeaeeee00f1de39405
0918nobita <nobita.0918@gmail.com>
2022-02-23 19:11:13 +0900

    :sparkles: 名前無しのフィールドで定義された構造体だけから derive できるようにした

--- crates/cli_rs/src/lib.rs
+++ crates/cli_rs/src/lib.rs
@@ -1,11 +1,23 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data, DeriveInput, Fields};
 
 #[proc_macro_derive(Arg)]
 pub fn derive_arg(input: TokenStream) -> TokenStream {
     let item = parse_macro_input!(input as DeriveInput);
+
+    let data_struct = match &item.data {
+        Data::Struct(data) => data,
+        _ => panic!("#[derive(Arg)] can only be applied to structs"),
+    };
+
+    let _unnamed_fields = match &data_struct.fields {
+        Fields::Unnamed(fields) => fields,
+        _ => panic!("#[derive(Arg)] can only be applied to structs with unnamed fields"),
+    };
+
     let struct_name = item.ident;
+
     quote! {
         impl #struct_name {
             fn answer() -> u32 { 42 }
