f67958ffb9f31c5bda44f1b98cc038cc5cfc95b9
0918nobita <nobita.0918@gmail.com>
2022-02-24 01:13:16 +0900

    :art: Improve readability

--- crates/cli-rs-macro/src/lib.rs
+++ crates/cli-rs-macro/src/lib.rs
@@ -2,23 +2,26 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, AttrStyle, Attribute, Data, DeriveInput, Fields};
 
+fn try_get_single_line_doc(attr: Attribute) -> Option<String> {
+    let tokens = match attr {
+        Attribute {
+            path,
+            style: AttrStyle::Outer,
+            tokens,
+            ..
+        } if path.is_ident("doc") => tokens,
+        _ => return None,
+    };
+    let doc = tokens
+        .into_iter()
+        .skip(1)
+        .collect::<proc_macro2::TokenStream>();
+    Some(format!("{}", doc).trim_matches('"').trim_start().to_owned())
+}
+
 fn extract_doc(attrs: impl Iterator<Item = Attribute>) -> String {
     attrs
-        .filter_map(|attr| {
-            if attr.style == AttrStyle::Outer && attr.path.is_ident("doc") {
-                let raw = format!(
-                    "{}",
-                    attr.tokens
-                        .into_iter()
-                        .skip(1)
-                        .collect::<proc_macro2::TokenStream>()
-                );
-                Some(raw.trim_matches('"').to_owned())
-            } else {
-                None
-            }
-        })
-        .map(|doc| doc.trim_start().to_owned())
+        .filter_map(try_get_single_line_doc)
         .collect::<Vec<_>>()
         .join("\n")
 }
