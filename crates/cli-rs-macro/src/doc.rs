b2ef166894e6a9ed132e510a21b7266f6e79bf76
0918nobita <nobita.0918@gmail.com>
2022-02-25 18:39:57 +0900

    :recycle: ドキュメンテーションコメントを取得する処理をリファクタリングした

--- crates/cli-rs-macro/src/doc.rs
+++ crates/cli-rs-macro/src/doc.rs
@@ -1,20 +1,15 @@
 use syn::Attribute;
 
 fn try_get_single_line_doc(attr: Attribute) -> Option<String> {
-    let tokens = match attr {
-        Attribute {
+    let meta = attr.parse_meta().ok()?;
+    match meta {
+        syn::Meta::NameValue(syn::MetaNameValue {
             path,
-            style: syn::AttrStyle::Outer,
-            tokens,
+            lit: syn::Lit::Str(lit_str),
             ..
-        } if path.is_ident("doc") => tokens,
-        _ => return None,
-    };
-    let doc = tokens
-        .into_iter()
-        .skip(1)
-        .collect::<proc_macro2::TokenStream>();
-    Some(format!("{}", doc).trim_matches('"').trim_start().to_owned())
+        }) if path.is_ident("doc") => Some(lit_str.value().trim_start().to_owned()),
+        _ => None,
+    }
 }
 
 pub fn extract_doc(attrs: impl Iterator<Item = Attribute>) -> String {
