a4a2b4f69c246808880261b0c95fc836345a0f50
0918nobita <nobita.0918@gmail.com>
2022-03-01 14:31:03 +0900

    :white_check_mark: upper_camel_to_kebab 関数のテストを追加した

--- crates/cli-rs-macro/src/kebab_case.rs
+++ crates/cli-rs-macro/src/kebab_case.rs
@@ -1,22 +1,25 @@
 pub fn upper_camel_to_kebab(str: &str) -> String {
-    fn folder((first_skipped, mut cs): (bool, Vec<char>), c: char) -> (bool, Vec<char>) {
-        if ('A'..='Z').contains(&c) {
-            if !first_skipped {
-                cs.push(c.to_ascii_lowercase());
-                return (true, cs);
-            }
-            cs.push('-');
-            cs.push(c.to_ascii_lowercase());
-            return (true, cs);
-        }
-
-        cs.push(c);
-        (first_skipped, cs)
-    }
-
     str.chars()
-        .fold((false, vec![]), folder)
-        .1
-        .into_iter()
+        .enumerate()
+        .flat_map(|(i, c)| {
+            if ('A'..='Z').contains(&c) {
+                if i != 0 {
+                    vec!['-', c.to_ascii_lowercase()]
+                } else {
+                    vec![c.to_ascii_lowercase()]
+                }
+            } else {
+                vec![c]
+            }
+        })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_upper_camel_to_kebab() {
+        assert_eq!(super::upper_camel_to_kebab("FooBar"), "foo-bar");
+        assert_eq!(super::upper_camel_to_kebab("fooBarBaz"), "foo-bar-baz");
+    }
 }
