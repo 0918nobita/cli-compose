ffb08eb7a21cdc2270f9e45837305411d9ae725e
0918nobita <nobita.0918@gmail.com>
2022-03-03 01:48:03 +0900

    :sparkles: FromKebabStr deriver をフィールド無しのバリアントしか持たない enum にのみ適用できるようにした

--- crates/cli-rs-macro/src/from_kebab_str.rs
+++ crates/cli-rs-macro/src/from_kebab_str.rs
@@ -1,17 +1,25 @@
 use proc_macro2::TokenStream;
 
+static INVALID_DERIVE_INPUT: &'static str =
+    "`#[derive(FromKebabStr)]` can only be applied to enums whose variants have no field";
+
 pub fn derive_from_kebab_str(input: TokenStream) -> syn::Result<TokenStream> {
     let derive_input = syn::parse2::<syn::DeriveInput>(input)?;
     let ty_name = &derive_input.ident;
-    let _data_enum = match &derive_input.data {
+    let data_enum = match &derive_input.data {
         syn::Data::Enum(data_enum) => data_enum,
         _ => {
-            return Err(syn::Error::new_spanned(
-                derive_input,
-                "`#[derive(FromKebabStr)]` can only be applied to enums",
-            ));
+            return Err(syn::Error::new_spanned(derive_input, INVALID_DERIVE_INPUT));
         }
     };
+    // TODO: 取り出した各識別子をもとに、from_str メソッドの定義コードを生成する
+    for variant in &data_enum.variants {
+        match &variant.fields {
+            syn::Fields::Unit => {}
+            _ => return Err(syn::Error::new_spanned(variant, INVALID_DERIVE_INPUT)),
+        }
+        let _ident = variant.ident.clone();
+    }
     Ok(quote::quote! {
         impl std::str::FromStr for #ty_name {
             type Err = ();
