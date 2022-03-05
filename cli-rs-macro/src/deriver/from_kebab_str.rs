f27889850b3bc9292c2f6d7dd5e1c04fafa8f537
0918nobita <nobita.0918@gmail.com>
2022-03-05 12:29:27 +0900

    :sparkles: FromKebabStr deriver を実装した

--- cli-rs-macro/src/deriver/from_kebab_str.rs
+++ cli-rs-macro/src/deriver/from_kebab_str.rs
@@ -1,31 +1,48 @@
+use convert_case::{Case, Casing};
 use proc_macro2::TokenStream;
+use quote::quote;
 
 static INVALID_DERIVE_INPUT: &str =
     "`#[derive(FromKebabStr)]` can only be applied to enums whose variants have no field";
 
 pub fn derive_from_kebab_str(input: TokenStream) -> syn::Result<TokenStream> {
     let derive_input = syn::parse2::<syn::DeriveInput>(input)?;
+
     let ty_name = &derive_input.ident;
+
     let data_enum = match &derive_input.data {
-        syn::Data::Enum(data_enum) => data_enum,
-        _ => {
-            return Err(syn::Error::new_spanned(derive_input, INVALID_DERIVE_INPUT));
-        }
-    };
-    // TODO: 取り出した各識別子をもとに、from_str メソッドの定義コードを生成する
+        syn::Data::Enum(data_enum) => Ok(data_enum),
+        _ => Err(syn::Error::new_spanned(&derive_input, INVALID_DERIVE_INPUT)),
+    }?;
+
+    let mut variants = Vec::<syn::Ident>::new();
+
     for variant in &data_enum.variants {
         match &variant.fields {
             syn::Fields::Unit => {}
-            _ => return Err(syn::Error::new_spanned(variant, INVALID_DERIVE_INPUT)),
+            _ => return Err(syn::Error::new_spanned(&variant, INVALID_DERIVE_INPUT)),
         }
-        let _ident = variant.ident.clone();
+
+        variants.push(variant.ident.clone());
     }
-    Ok(quote::quote! {
+
+    let arms = variants
+        .iter()
+        .map(|variant| {
+            let variant_str = variant.to_string().to_case(Case::Kebab);
+            quote! { #variant_str => Ok(#ty_name::#variant), }
+        })
+        .collect::<TokenStream>();
+
+    Ok(quote! {
         impl std::str::FromStr for #ty_name {
             type Err = ();
 
             fn from_str(s: &str) -> Result<Self, Self::Err> {
-                todo!()
+                match s {
+                    #arms
+                    _ => Err(()),
+                }
             }
         }
     })
