b77d7421ce6f27e5bf1422fd122fa8950b93a190
0918nobita <nobita.0918@gmail.com>
2022-03-05 13:12:34 +0900

    :white_check_mark: extract_meta 関数のテストを追加した

--- cli-rs-macro/src/attr_meta.rs
+++ cli-rs-macro/src/attr_meta.rs
@@ -13,3 +13,36 @@ where
         })
         .flatten()
 }
+
+#[cfg(test)]
+mod tests {
+    use quote::ToTokens;
+    use syn::parse::Parser;
+
+    use super::extract_meta;
+
+    fn extract_meta_str<'a, A>(attrs: A, name: &'a str) -> Vec<String>
+    where
+        A: Iterator<Item = &'a syn::Attribute> + 'a,
+    {
+        extract_meta(attrs, name)
+            .map(|nested_meta| nested_meta.to_token_stream().to_string())
+            .collect::<Vec<_>>()
+    }
+
+    #[test]
+    fn test_extract_meta() -> syn::Result<()> {
+        let attrs = syn::Attribute::parse_inner.parse2(quote::quote! {
+            #![foo(num = 42)]
+            #![bar(str = "hello")]
+            #![baz(path)]
+        })?;
+
+        let foo = extract_meta_str(attrs.iter(), "foo");
+        let bar = extract_meta_str(attrs.iter(), "bar");
+        let baz = extract_meta_str(attrs.iter(), "baz");
+
+        insta::assert_debug_snapshot!(vec![foo, bar, baz]);
+        Ok(())
+    }
+}
