d7006a5cea21f90b781c8f7dd7b9cc1e8c793284
0918nobita <nobita.0918@gmail.com>
2022-03-12 14:16:35 +0900

    :sparkles: define_cli マクロで複数個の「型名->型名」を受け取れるようにした

--- cli-compose-macro/src/define_cli.rs
+++ cli-compose-macro/src/define_cli.rs
@@ -11,7 +11,7 @@ impl parse::Parse for CliDef {
     fn parse(input: parse::ParseStream) -> syn::Result<Self> {
         let cli_ty = input.parse()?;
 
-        input.parse::<syn::Token![,]>()?;
+        input.parse::<syn::Token![->]>()?;
 
         let res_ty = input.parse()?;
 
@@ -19,29 +19,54 @@ impl parse::Parse for CliDef {
     }
 }
 
-pub fn define_cli(input: TokenStream) -> syn::Result<TokenStream> {
-    let CliDef { cli_ty, res_ty } = syn::parse2(input)?;
+struct CliDefs(Vec<CliDef>);
 
-    let contents = quote! {
-        #[allow(dead_code)]
-        struct #res_ty {
-            input: String,
-            output: Option<String>,
-            stdin: Option<playground_opts::StdinOpt>,
-            stdout: Option<playground_opts::StdoutOpt>,
-            verbose: Option<playground_opts::Verbose>,
-        }
+impl<'a> std::iter::IntoIterator for &'a CliDefs {
+    type Item = &'a CliDef;
+    type IntoIter = std::slice::Iter<'a, CliDef>;
 
-        #[allow(dead_code)]
-        impl cli_compose::runtime::AsCli<#res_ty> for #cli_ty {
-            fn parse(args: impl Iterator<Item = String>) -> #res_ty {
-                let tokens = cli_compose::runtime::parse_into_tokens(args).collect::<Vec<_>>();
-                println!("{:?}", tokens);
-                todo!()
-            }
-        }
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter()
     }
-    .to_string();
+}
+
+impl parse::Parse for CliDefs {
+    fn parse(input: parse::ParseStream) -> syn::Result<Self> {
+        let defs = input
+            .parse_terminated::<CliDef, syn::Token![,]>(CliDef::parse)?
+            .into_iter()
+            .collect::<Vec<_>>();
+        Ok(Self(defs))
+    }
+}
+
+pub fn define_cli(input: TokenStream) -> syn::Result<TokenStream> {
+    let defs: CliDefs = syn::parse2(input)?;
+
+    let contents = defs.into_iter()
+        .map(|CliDef { cli_ty, res_ty }| {
+            quote! {
+                #[allow(dead_code)]
+                struct #res_ty {
+                    input: String,
+                    output: Option<String>,
+                    stdin: Option<playground_opts::StdinOpt>,
+                    stdout: Option<playground_opts::StdoutOpt>,
+                    verbose: Option<playground_opts::Verbose>,
+                }
+
+                #[allow(dead_code)]
+                impl cli_compose::runtime::AsCli<#res_ty> for #cli_ty {
+                    fn parse(args: impl Iterator<Item = String>) -> #res_ty {
+                        let tokens = cli_compose::runtime::parse_into_tokens(args).collect::<Vec<_>>();
+                        println!("{:?}", tokens);
+                        todo!()
+                    }
+                }
+            }
+        })
+        .collect::<TokenStream>()
+        .to_string();
 
     Ok(quote! {
         let out_dir = std::env::var("OUT_DIR").expect("$OUT_DIR is not set");
