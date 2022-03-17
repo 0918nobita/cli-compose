1d665827e45bbf98bab4535f227035cedb5d96aa
0918nobita <nobita.0918@gmail.com>
2022-03-17 15:18:38 +0900

    :white_check_mark: モジュールの公開/非公開に関するテストを追加した

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,20 @@
 #[cfg(any(feature = "schema", feature = "codegen"))]
 mod schema_impl;
 
+#[cfg_attr(
+    all(feature = "schema", not(feature = "codegen"), not(feature = "runtime")),
+    doc = r##"
+```compile_fail
+// Cannot use `runtime` mod without `runtime` feature
+use cli_compose::runtime::*;
+```
+
+```compile_fail
+// Cannot use `codegen` mod without `codegen` feature
+use cli_compose::codegen::*;
+```
+"##
+)]
 #[cfg(feature = "schema")]
 pub mod schema {
     pub use crate::schema_impl::*;
@@ -17,8 +31,36 @@ pub mod schema {
     }
 }
 
+#[cfg_attr(
+    all(feature = "codegen", not(feature = "runtime"), not(feature = "schema")),
+    doc = r##"
+```compile_fail
+// Cannot use `runtime` mod without `runtime` feature
+use cli_compose::runtime::*;
+```
+
+```compile_fail
+// Cannot use `schema` mod without `schema` feature
+use cli_compose::schema::*;
+```
+"##
+)]
 #[cfg(feature = "codegen")]
 pub mod codegen;
 
+#[cfg_attr(
+    all(feature = "runtime", not(feature = "codegen"), not(feature = "schema")),
+    doc = r##"
+```compile_fail
+// Cannot use `codegen` mod without `codegen` feature
+use cli_compose::codegen::*;
+```
+
+```compile_fail
+// Cannot use `schema` mod without `schema` feature
+use cli_compose::schema::*;
+```
+"##
+)]
 #[cfg(feature = "runtime")]
 pub mod runtime;
